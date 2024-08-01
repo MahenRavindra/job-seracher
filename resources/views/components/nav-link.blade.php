452c944a76f5e99750f55392981dc98ad20e9fb7
Mahen Ravindra <24ravindraicrox@gmail.com>
2024-08-02 02:08:07 +0530

    start learn blade

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,4 @@
+<a href="/"
+    class="{{ request()->is('/') ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white' }}">
+    {{ $slot }}
+</a>
